d6296d3df8f4632716423e67266c3230e5863f49
Heart-State <863855216@qq.com>
2024-03-09 17:00:39 +0800

    歌单详情

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ffcb8e51'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5nvt41kpib"
+    "revision": "0.d6rsi8lfi7"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/PlayList.vue
+++ src/components/PlayList.vue
@@ -1,47 +1,70 @@
 <script setup>
 import { onMounted, onUnmounted, ref } from 'vue'
 import { useEventListener } from '../assets/js/event'
-import ColorThief from '../../node_modules/colorthief/dist/color-thief.mjs'
 import { useRoute } from 'vue-router';
 import axios from '../axios/axios';
 
 const route = useRoute()
-const colorThief = new ColorThief();
+const playListInfo = ref()
+const playlist = ref()
 //dom
 const header = ref(null)
 const cover = ref(null)
 const bg = ref(null)
 const imageElement = ref(null)
-let rgb
 
+
+//获取歌单详情
+const getListInfo = async () => {
+    const { data: res } = await axios.get('/playlist/detail?id=' + route.query.id)
+    if (res.code != 200) {
+        Message({ message: res.msg })
+        return
+    }
+    playListInfo.value = res.playlist
+}
 
 //获取歌曲
 const getList = async () => {
-    const { data: res } = await axios.get('')
+    const { data: res } = await axios.get(`/playlist/track/all?id=${route.query.id}&limit=${playListInfo?.trackCount}&offset=0`)
+    if (res.code != 200) {
+        Message({ message: res.msg })
+        return
+    }
+    playlist.value = res.songs
 }
 
 //监听滚动改变顶部背景颜色
 useEventListener(document, "scroll", (e) => {
     let scrollTop = document.documentElement.scrollTop
     let range = cover.value.offsetHeight
-    if (scrollTop <= range) {
-        header.value.style.backgroundColor = 'rgba(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ',' + scrollTop / range + ')'
+    let rgb = ''
+    if (document.documentElement.className.indexOf("dark") > -1) {
+        rgb = '0,0,0,'
     } else {
-        header.value.style.backgroundColor = 'rgba(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ',1)'
+        rgb = '255,255,255,'
+    }
+    if (scrollTop <= range) {
+        header.value.style.backgroundColor = 'rgba(' + rgb + + scrollTop / range + ')'
+    } else {
+        header.value.style.backgroundColor = 'rgba(' + rgb + '0.98)'
     }
 })
 
 onMounted(() => {
-    imageElement.value.onload = () => {
-        rgb = colorThief.getColor(imageElement.value)
-        bg.value.style.backgroundColor = 'rgba(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ',1)'
-    }
+    //获取歌单详情
+    getListInfo()
+    //获取歌曲
+    getList()
 })
 </script>
 
 <template>
-    <div ref="bg" class="fixed z-0 w-screen h-screen dark:brightness-75"></div>
-    <div class="dark:text-white flex flex-col  min-h-screen z-10">
+    <div ref="bg" class="fixed z-0 w-screen h-screen dark:brightness-75">
+        <img ref="imageElement" class="w-full h-full object-cover blur-[100px]" :src="playListInfo?.coverImgUrl"
+            alt="bg">
+    </div>
+    <div class="dark:text-white flex flex-col min-h-screen z-10">
         <div ref="header" class="header p-1 flex justify-between sticky top-0 z-50 dark:brightness-75">
             <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -49,7 +72,7 @@ onMounted(() => {
                     @click="$router.back()">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                 </svg>
-                <h6 class="pl-1">歌单</h6>
+                <h6 class="pl-1">{{ playListInfo?.name }}</h6>
             </div>
             <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -66,13 +89,12 @@ onMounted(() => {
             </div>
         </div>
         <div ref="cover" class="py-10 z-10 text-center">
-            <img ref="imageElement" class="w-28 h-28 object-cover rounded-lg m-auto bg-blue-300"
-                src="http://p2.music.126.net/Cl0-NpZ0ESTDjJ1HmZ33KA==/109951163460576279.jpg" alt="cover"
-                crossorigin="anonymous">
-            <h5>koto</h5>
-            <p class="text-xs text-gray-500">Redamancy</p>
+            <img ref="imageElement" class="w-28 h-28 object-cover rounded-lg m-auto" :src="playListInfo?.coverImgUrl"
+                alt="cover">
+            <h5>{{ playListInfo?.name }}</h5>
+            <p class="text-xs text-gray-500">{{ playListInfo?.creator.nickname }}</p>
         </div>
-        <div class="flex-auto rounded-t-2xl bg-white opacity-90 dark:bg-black dark:opacity-50 py-3 z-10">
+        <div class="flex-auto rounded-t-2xl bg-white opacity-90 dark:bg-black py-3 z-10">
             <div class="flex justify-between px-3 py-2 sticky top-12 bg-white opacity-98 dark:bg-black">
                 <div class="flex">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -84,7 +106,7 @@ onMounted(() => {
                     </svg>
                     <p class="pl-1">
                         播放全部
-                        <span class="text-xs text-gray-400">(10)</span>
+                        <span class="text-xs text-gray-400">({{ playListInfo?.trackCount }})</span>
                     </p>
                 </div>
                 <div>
@@ -96,19 +118,24 @@ onMounted(() => {
                 </div>
             </div>
             <div class="pt-1">
-                <div v-for="i in 100" class="flex items-center text-sm py-1.5 px-3 first:pt-0">
+                <div v-for="song in playlist" :key="index" class="flex items-center text-sm py-1.5 px-3 first:pt-0">
                     <div class="w-5 text-center text-gray-400">
-                        <p>{{ i }}</p>
+                        <p>{{ index }}</p>
                     </div>
                     <div class="flex-1 pl-3">
-                        <h6>小半</h6>
+                        <h6>{{ song.name }}</h6>
                         <p class="text-xs flex">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                                 class="w-4 h-4 text-indigo-500" v-if="true">
                                 <path
                                     d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                             </svg>
-                            <span>陈粒—小梦大半</span>
+                            <span>
+                                <span v-for="item in song.ar">
+                                    {{ item.name }} 
+                                </span>
+                                —小梦大半
+                            </span>
 
                         </p>
                     </div>
